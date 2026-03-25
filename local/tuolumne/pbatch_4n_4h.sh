a7fac6bd448f1f3687476f83e216fb603db98f30
Barry <rountree4@llnl.gov>
2026-03-25 15:04:27 -0700

    Hardening.
    Assume that env.sh has been sourced prior to running any script,
    and complain (and exit) if it hasn't been.
    
    Adds new environment variable SPINDLE_SCRIPTS.
    
    Should allow for running scripts from the root of the repo, thus
    avoiding problems of the "local" directory disappearing as we
    switch to the feature branch.

--- local/tuolumne/pbatch_4n_4h.sh
+++ local/tuolumne/pbatch_4n_4h.sh
@@ -1,3 +1,8 @@
+if [[ -v SPINDLE_SCRIPTS ]]; then
+    echo "Using scripts in " $SPINDLE_SCRIPTS
+else
+    echo "SPINDLE_SCRIPTS not set, please source env.h.  Exiting."
+fi
 flux alloc --queue=pbatch --time-limit=4h --nodes=4 --exclusive
 
 
