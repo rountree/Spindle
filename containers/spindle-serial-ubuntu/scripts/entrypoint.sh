206a33e3e1094666914bf1c9568bc5b3695cbd74
Barry <rountree4@llnl.gov>
2026-04-16 16:50:22 -0700

    Containers:  enables core files.

--- containers/spindle-serial-ubuntu/scripts/entrypoint.sh
+++ containers/spindle-serial-ubuntu/scripts/entrypoint.sh
@@ -2,3 +2,15 @@ printf "\nStarting munged\n"
 
 sudo -u munge /usr/sbin/munged --foreground
 
+# Turn off apport so we're not fighting over kernel.core_pattern
+printf "\nEnabling corefiles\n"
+if pidof systemd > /dev/null 2>&1; then
+    if systemctl is-active --quiet apport 2>/dev/null; then
+        sudo systemctl stop apport
+    fi
+fi
+sudo sysctl -w kernel.core_pattern='%E.%e.%p.%t.core'
+echo 'ulimit -c unlimited' | sudo tee /etc/profile.d/coredump.sh
+echo 'ulimit -c unlimited' | sudo tee -a /etc/bash.bashrc
+
+
