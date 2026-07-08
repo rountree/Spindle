92edea38870e61c713dd3e3da517f9d953144c5f
Barry Rountree <rountree4@llnl.gov>
2026-07-08 12:11:56 -0700

    [blr-scripts] Updates clean and nuke.
    Both now handle all four resource managers at once.

--- local/LC/clean.sh
+++ local/LC/clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 if ! declare -F require_spindle_env >/dev/null; then
     printf 'Please source env.sh in this shell before running this script.\n' >&2
     exit 1
@@ -9,7 +10,19 @@ export DEBUG=1
 export VERBOSE=1
 export V=1
 
-cd "$SPINDLE_BUILD" || exit 1
-make clean -j 2>&1 | tee ./clean.out
-cd - >/dev/null || exit 1
 
+cd "$SPINDLE_FLUX_BUILD" 
+make clean -j 2>&1 | ts 'flux   %Y-%m-%d %H:%M:%S'| tee ./build.out
+cd - >/dev/null 
+
+cd "$SPINDLE_SERIAL_BUILD" 
+make clean -j 2>&1 | ts 'serial %Y-%m-%d %H:%M:%S'| tee ./build.out
+cd - >/dev/null 
+
+cd "$SPINDLE_SLURM_BUILD" 
+make clean -j 2>&1 | ts 'slurm  %Y-%m-%d %H:%M:%S'| tee ./build.out
+cd - >/dev/null 
+
+cd "$SPINDLE_PLUGIN_BUILD" 
+make clean -j 2>&1 | ts 'plugin %Y-%m-%d %H:%M:%S'| tee ./build.out
+cd - >/dev/null 
--- local/LC/nuke.sh
+++ local/LC/nuke.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -euxo pipefail
 if ! declare -F require_spindle_env >/dev/null; then
     printf 'Please source env.sh in this shell before running this script.\n' >&2
     exit 1
 fi
 check_spindle_tag || exit 1
 
-rm -rf $SPINDLE_BUILD $SPINDLE_INSTALL
+rm -rf $SPINDLE_FLUX_BUILD \
+       $SPINDLE_FLUX_INSTALL \
+       $SPINDLE_SERIAL_BUILD \
+       $SPINDLE_SERIAL_INSTALL \
+       $SPINDLE_SLURM_BUILD \
+       $SPINDLE_SLURM_INSTALL \
+       $SPINDLE_PLUGIN_BUILD \
+       $SPINDLE_PLUGIN_INSTALL
+
